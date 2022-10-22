7d1680c04227f53bcb1ea32b1c104854dbd9cdd6
tkrystian <t.krystian@gmail.com>
2022-10-22 14:18:28 +0200

    cos

--- src/pl/camp/it/git/App.java
+++ src/pl/camp/it/git/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("GIT!!");
         System.out.println("cos nowego");
+        System.out.println("jeszcze cos nowego");
     }
 }
